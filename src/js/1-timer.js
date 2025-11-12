314740868947c74f2899787a42edd91ded467446
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2025-11-12 23:05:02 +0200

    update build

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,14 @@
-// import flatpickr from 'flatpickr';
+import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+
+// Завдання
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
