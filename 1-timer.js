2724b69f8f473d97c2cbd381879b64bf3452b3d2
Shoni182 <100448741+Shoni182@users.noreply.github.com>
2025-11-14 09:37:28 +0000

    Deploying to gh-pages from @ Shoni182/goit-js-hw-10@80a7190699a0910c94c2178712d53927a19ab59f 🚀

---
+++ 1-timer.js
@@ -0,0 +1,3 @@
+import"./assets/main-CJWCQS9G.js";import{i as t,f as o}from"./assets/vendor-8AtNpez9.js";t.show({title:"Hey",message:"What would you like to add?"});const i={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]);//! тут обробляти дату вибрану користувачем
+}};o("#datetime-picker",i);
+//# sourceMappingURL=1-timer.js.map
