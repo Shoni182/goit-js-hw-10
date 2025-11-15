557f2be6841680fc2c3c9ec7620b6439d55566a6
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2025-11-15 15:27:28 +0200

    update task 1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,11 +3,12 @@ import iziToast from 'izitoast';
 
 // Завдання
 
-let userSelectedDate = null;
-
+// #region Task
 // - Стилізація
 
 // - Пошук елементів: кнопка старт, інпут
+
+// В 1 завданні обов’язково кнопка Старт має бути неактивною одразу при завантаженні сторінки, коли йде відлік часу і при обиранні невалідної дати. Інпут треба робити неактивним, коли йде відлік часу, коли таймер доходить до 00 - знов робимо інпут активним, щоб можна було обрати нову дату.
 
 // - Вибір Дати
 // записати вибрану дату після валідації її в методі
@@ -22,6 +23,11 @@ let userSelectedDate = null;
 // оновлення інтерфейс таймера textContent
 // таймер зупиняєтья коли дійшов до кінцевої дати тобто 00 00 00 00
 
+//  щоб має зробити таймер щоб рахувати
+//  він повинен знати першу вибрану дату в млс в майбутньому
+//  він повинен знати теперішню дату в мілісекундаї
+// цб різницю перетворити в години дні за допомогою конвертації convertMs(ms)
+
 // - Форматування часу
 // функція convertMs( ) повертає обʼєкт з рохрахованим часом що залишився докінцевох лати
 // Напиши функцію, наприклад addLeadingZero(value), яка використовує метод рядка padStart() і перед відмальовуванням інтерфейсу форматує значення.
@@ -30,10 +36,29 @@ let userSelectedDate = null;
 // - Бібліотека повідомлень
 // Повідомлення
 
-iziToast.show({
-  title: 'Hey',
-  message: 'What would you like to add?',
-});
+// #endregion
+
+//!  ======== Пошук ДОМ елементів ========
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  input: document.querySelector('#datetime-picker[data-active]'),
+  clockFace: document.querySelector('.value'),
+
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
+};
+
+//! - ======== button settings ========
+
+refs.startBtn.dataset.start = 'disable';
+
+//! - ======== data pick ========
+// записати сюди дату
+let currentTime;
+let userSelectedDate = null;
 
 const options = {
   enableTime: true,
@@ -41,30 +66,97 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]); //! тут обробляти дату вибрану користувачем
-    // створити валідацію - дата повинна бути лише в майбутньому
+    // console.log(selectedDates[0]);
+    currentTime = new Date();
+
+    if (selectedDates[0] > currentTime) {
+      // console.log('Option - true ');
+      refs.startBtn.dataset.start = 'enable';
+
+      userSelectedDate = selectedDates[0];
+    } else {
+      console.log('Option - false ');
+      refs.startBtn.dataset.start = 'disable';
+
+      iziToast.show({
+        title: 'Hey',
+        message: `Please choose a date in the future! for exapmle ${new Date(
+          Date.now() + 76000000
+        ).toLocaleString('uk-UA')}`,
+        position: 'center',
+        close: true,
+        closeOnEscape: true,
+      });
+      // console.log('false');
+    }
   },
 };
 
-flatpickr('#datetime-picker', options);
 // lib init
+flatpickr('#datetime-picker', options);
 
-// - Підрахунок значенб
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
+//! - ======== timer ========
 
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+const timer = {
+  isActive: false,
+  intervalId: null,
 
-//   return { days, hours, minutes, seconds };
-// }
+  start() {
+    if (this.isActive) return;
+    this.isActive = true;
+    refs.input.dataset.active = 'disable';
+    refs.startBtn.dataset.start = 'disable';
+
+    this.intervalId = setInterval(() => {
+      currentTime = new Date();
+      const diffMS = userSelectedDate - currentTime;
+      const result = convertMs(diffMS);
+      console.log(result);
+
+      refs.days.textContent = result.days;
+      refs.hours.textContent = result.hours;
+      refs.minutes.textContent = result.minutes;
+      refs.seconds.textContent = result.seconds;
+
+      if (diffMS <= 0) {
+        this.stop();
+      }
+    }, 1000);
+  },
+
+  stop() {
+    if (!this.isActive) return;
+    this.isActive = false;
+    // console.log('STOP TIMER');
+    clearInterval(this.intervalId);
+    refs.clockFace.textContent = '00:00:00:00';
+    refs.input.dataset.active = 'disable';
+  },
+};
+
+//! - ======== init ========
+
+refs.startBtn.addEventListener('click', () => timer.start());
+
+//! - ======== Функція преведеннчя мс ========
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const pad = num => String(num).padStart(2, '0');
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
