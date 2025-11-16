6d4f2fe2f54dd00d3f16ff0bfab8d046f033974e
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2025-11-16 15:42:34 +0200

    add task 2 in progress

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,11 @@
 import iziToast from 'izitoast';
+
+// : Пошук DOM
+
+refs = {
+  input: document.querySelector('label[name="delay"]'),
+  radioFullfiled: document.querySelector('input[value="fulfilled"]'),
+  radioRejected: document.querySelector('input[value="rejected"]'),
+};
+
+// let delayInput =
